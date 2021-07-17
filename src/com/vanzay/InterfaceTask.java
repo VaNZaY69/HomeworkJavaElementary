399c15aabdb83d694e39891fd6e22d838592f9d7
Ivan Zaets <ivan.i.zaets@gmail.com>
2021-07-17 16:06:47 +0300

    create InterfaceTask and Task, change code

---
+++ src/com/vanzay/InterfaceTask.java
@@ -0,0 +1,12 @@
+package com.vanzay;
+
+public interface AbstractTask {
+
+    public void Task(String task);
+
+    public String setState(String state);
+
+    public String getTask(String task);
+
+    public String getState(String state);
+}
---
+++ src/com/vanzay/ListCompletedTask.java
@@ -0,0 +1,2 @@
+package com.vanzay;public class ListCompletedTask {
+}
